72fc00db05abc45e12891077a2ee118c290183c1
gavengao <gavengao@tencent.com>
2025-02-13 15:02:17 +0800

    user

--- apps/user/internal/constant/login.go
+++
@@ -1,6 +0,0 @@
-package constant
-
-const (
-	RegisterTypeNormal = "normal"
-	RegisterTypeWechat = "wechat"
-)
--- apps/user_gjw/biz/dal/init.go
+++
@@ -1,8 +0,0 @@
-package dal
-
-import "github.com/lzl-here/bt-shop-backend/apps/user_gjw/biz/dal/mysql"
-
-func Init() {
-	// redis.Init()
-	mysql.Init()
-}
